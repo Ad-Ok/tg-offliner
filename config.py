782164127045d7382567a1b736cb41c05b1b2a6b
Ad-Ok <adolf_oknov@mail.ru>
2025-04-10 01:44:59 +0300

    remove chat from settings

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 PHONE = os.getenv("PHONE")
-CHAT_USERNAME = os.getenv("CHAT_USERNAME")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "telegram_export")
 
