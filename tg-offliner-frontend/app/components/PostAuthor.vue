03c44bafa08475517572130a327fe48745be3e60
Ad-Ok <adolf_oknov@mail.ru>
2025-08-22 15:55:35 +0300

    add default avatar

--- tg-offliner-frontend/app/components/PostAuthor.vue
+++ tg-offliner-frontend/app/components/PostAuthor.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="author">
-    <img v-if="avatar" :src="avatarSrc" alt="Avatar" />
+    <div class="avatar-wrapper">
+      <img v-if="avatar" :src="avatarSrc" alt="Avatar" class="avatar-image" />
+    </div>
     <a v-if="link" :href="link" target="_blank">{{ name }}</a>
     <span v-else>{{ name }}</span>
   </div>
