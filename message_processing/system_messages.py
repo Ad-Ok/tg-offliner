ecd34d2273d48e5980f6f1661f44c082a3ed1e28
Ad-Ok <adolf_oknov@mail.ru>
2025-10-18 02:09:50 +0300

    cleanup: delete unusing files

--- message_processing/system_messages.py
+++
@@ -1,5 +0,0 @@
-def process_system_message(message):
-    """Обрабатывает системное сообщение."""
-    if message.action:
-        return f"Системное сообщение: {type(message.action).__name__}"
-    return None
